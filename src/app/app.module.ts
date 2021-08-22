791f944110482154200a2580a17b6a9bd06819ee
Anamika.Sawhney <anamika.9.sawhney@niit.com>
2021-08-22 19:19:50 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
-
+import { NewsService } from  '../app/services/news.service';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [ AppComponent, HeaderComponent, FooterComponent, DashboardComponent, CardComponent ],
+  imports: [ BrowserModule, BrowserAnimationsModule, HttpClientModule, FormsModule,
+    MatCardModule, MatButtonModule, MatInputModule, MatExpansionModule, MatToolbarModule,
+    MatFormFieldModule ],
+  providers: [ NewsService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
